e1db0f84f4c6603b913982fca9792ab185509e0c
Miles Higbie <miles.higbie@gmail.com>
2020-02-17 15:11:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file
 
 function findMatching(drivers, str){
-  drivers.filter(function (el) {el == str})
+  const newDrivers = drivers.filter(function (el) {el == str})
+  return newDrivers
 }
