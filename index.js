542da3b934e59746088961dd6892aa6ebb51971e
Miles Higbie <miles.higbie@gmail.com>
2020-02-17 15:17:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Code your solution in this file
 
 function findMatching(drivers, str){
-  drivers.filter(function (el) {return el == str})
+  drivers.filter(function (el) {
+    return el.toLowerCase() === str.toLowerCase()
+  })
 }
+
